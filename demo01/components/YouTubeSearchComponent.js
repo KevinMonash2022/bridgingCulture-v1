ece224c0a90e5b550af7e1121cdcd0872ae6c87d
KAIXIN ZHANG <145351389+KevinMonash2022@users.noreply.github.com>
2024-04-26 16:31:44 +1000

    Update YouTubeSearchComponent.js

--- demo01/components/YouTubeSearchComponent.js
+++ demo01/components/YouTubeSearchComponent.js
@@ -1,6 +1,3 @@
-
-
-User
 import React, { useState } from 'react';
 import axios from 'axios';
 
@@ -9,6 +6,10 @@ const YouTubeSearchComponent = () => {
     const [videos, setVideos] = useState([]);
 
     const fetchVideos = async () => {
+        if (!searchTerm) {
+            console.log('No search term provided');
+            return;
+        }
         try {
             const response = await axios.get(`https://www.googleapis.com/youtube/v3/search`, {
                 params: {
@@ -16,7 +17,7 @@ const YouTubeSearchComponent = () => {
                     maxResults: 10,
                     q: searchTerm,
                     type: 'video',
-                    key: 'AIzaSyB8kOmxv5TbI6aA8tCC_hLL7U0vs1DxqkA'  // Be sure to replace 'YOUR_API_KEY' with your actual YouTube API key.
+                    key: 'AIzaSyB8kOmxv5TbI6aA8tCC_hLL7U0vs1DxqkA'
                 }
             });
             setVideos(response.data.items);
@@ -24,6 +25,7 @@ const YouTubeSearchComponent = () => {
             console.error('Error fetching videos:', error);
         }
     };
+
 
     return (
         <div className="mt-4">
